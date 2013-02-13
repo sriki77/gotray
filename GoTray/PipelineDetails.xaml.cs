b415eec4f660577ca012d5444baab12c0c466e6f
Srikanth <sriki77@gmail.com>
2013-02-13 08:08:27 +0530

    Implementing pipeline view model

--- GoTray/PipelineDetails.xaml.cs
+++ GoTray/PipelineDetails.xaml.cs
@@ -1,52 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using GoTray.Common;
+using GoTrayFeed;
 using Windows.UI.Xaml.Navigation;
-
-// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
 namespace GoTray
 {
-    /// <summary>
-    /// A basic page that provides characteristics common to most applications.
-    /// </summary>
-    public sealed partial class PipelineDetails : GoTray.Common.LayoutAwarePage
+    public sealed partial class PipelineDetails : LayoutAwarePage
     {
         public PipelineDetails()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Populates the page with content passed during navigation.  Any saved state is also
-        /// provided when recreating a page from a prior session.
-        /// </summary>
-        /// <param name="navigationParameter">The parameter value passed to
-        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
-        /// </param>
-        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
-        /// session.  This will be null the first time a page is visited.</param>
-        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            base.OnNavigatedTo(e);
+            var project = e.Parameter as GoProject;
+            var pdm = new PiplineDetailsModel(project);
         }
 
-        /// <summary>
-        /// Preserves state associated with this page in case the application is suspended or the
-        /// page is discarded from the navigation cache.  Values must conform to the serialization
-        /// requirements of <see cref="SuspensionManager.SessionState"/>.
-        /// </summary>
-        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
-        protected override void SaveState(Dictionary<String, Object> pageState)
+        public class PiplineDetailsModel
         {
+            public PiplineDetailsModel(GoProject project)
+            {
+                PipelineName = project.PipelineName;
+                LastBuildTime = project.LastBuildTime;
+            }
+
+            public string PipelineName { get; private set; }
+            public string[] Stages { get; private set; }
+            public string LastBuildTime { get; private set; }
+
         }
     }
 }
